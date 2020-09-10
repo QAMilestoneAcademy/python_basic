a27c518fc59daf1353990e76674ab11c6fae4802
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-09-10 12:39:41 +0400

    test

---
+++ Class4/boolean_data_type.py
@@ -0,0 +1,19 @@
+
+i=True
+k=False
+#Comparison operators
+print(32>24)
+print(24<32)
+print(7==7)
+print(6!=7)
+print(24<=32)
+print(32>=24)
+
+age=32
+print(age==34)
+
+# in operator
+name="Siyona"
+print("d" in name)
+name2="Tazine"
+print("d" not in name2)
