2dcbae1b8878c0eb015cd2c237418989d5f8fd8b
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-10-10 08:43:49 +0400

    test

---
+++ Class2/excercise_mathsoperation_userinput.py
@@ -0,0 +1,10 @@
+# Task 3 - Take 2 numbers as input from  user .
+# Convert to int & perform mathmatical operation
+num1=int(input("Enter num1:\t"))
+num2=int(input("Enter num2:\t"))
+print("add",num1+num2)
+print("subtract",num1*num2)
+print("divide",num1/num2)
+print("remainder",num1%num2)
+print("quotient",num1//num2)
+print("power",num1**num2)
