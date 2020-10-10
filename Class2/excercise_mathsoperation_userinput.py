0cc3b662f713cf3aae22ff534dff9d35104b2fc3
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-10-10 08:44:53 +0400

    test

--- Class2/excercise_mathsoperation_userinput.py
+++ Class2/excercise_mathsoperation_userinput.py
@@ -1,5 +1,6 @@
 # Task 3 - Take 2 numbers as input from  user .
 # Convert to int & perform mathmatical operation
+#Using boolean if number1 greater than 100 by returning true or false
 num1=int(input("Enter num1:\t"))
 num2=int(input("Enter num2:\t"))
 print("add",num1+num2)
