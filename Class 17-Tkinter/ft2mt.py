b73d34097b66275f6bbd21b309841a4bb73ce083
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-10-19 10:11:58 +0400

    test

---
+++ Class 17-Tkinter/ft2mt.py
@@ -0,0 +1,51 @@
+from tkinter import Tk, Button, Label,DoubleVar,Entry
+
+window=Tk()
+window.title('Feet to Meter Conversion')
+window.configure(background="light blue"
+                            "")
+window.geometry("320x220")
+window.resizable(width=False,height=False)
+
+def convert():
+    value=float(ft_entry.get())
+    meter=value*0.3048
+    mt_value.set(round(meter,4))
+
+def clear():
+    ft_value.set("")
+    mt_value.set("")
+
+
+
+#Lable A widget used to display text on the screen
+ft_lbl=Label(window,text="Feet",bg="blue",fg="yellow",width=14)
+ft_lbl.grid(column=0,row=0,padx=15,pady=15)
+
+#DoubleVar type value allows float
+ft_value=DoubleVar()
+#Entry -A text entry widget that allows only a single line of text
+ft_entry=Entry(window,textvariable=ft_value,width=14)
+ft_entry.grid(column=1,row=0)
+#clearing the screen before start
+ft_entry.delete(0,'end')
+
+mt_lbl=Label(window,text="Meter",bg="blue",fg="yellow",width=14)
+mt_lbl.grid(column=0,row=1,padx=15,pady=15)
+
+
+
+#DoubleVar type value allows float
+mt_value=DoubleVar()
+#Entry -A text entry widget that allows only a single line of text
+mt_entry=Entry(window,textvariable=mt_value,width=14)
+mt_entry.grid(column=1,row=1)
+#clearing the screen before start
+mt_entry.delete(0,'end')
+
+convert_btn=Button(window,text="Convert",bg="black",fg="white",width=14,command=convert)
+convert_btn.grid(column=0,row=3,padx=15)
+clear_btn=Button(window,text="Clear",bg="Dark Grey",fg="white",width=14,command=clear)
+clear_btn.grid(column=1,row=3,padx=15)
+
+window.mainloop()
--- Class17-18-19-functions/function_part1.py
+++ Class17-18-19-functions/function_part1.py
@@ -1,33 +1,34 @@
-#a,b parameters
-def add(a,b):
-    sum=a+b
-    return sum
-
-#arguments
-my_sum=add(100,300)
-print(my_sum+50)
-
-def multiply(x,y):
-    multiple=x*y
-    return multiple
-
-my_multiple=multiply(13,14)
-print(my_multiple)
-
+# def add(a,b):
+#     sum=a+b
+#     return sum
+#
+# my_sum=add(29,34)
+# print(my_sum+2)
+# print(add(290,340))
+#
+#
+# def multiply(x,y):
+#     multiple=x*y
+#     return multiple
+#
+# my_multiple=multiply(13,14)
+# print(my_multiple)
+#
 #action based function
 def greetings(daytime,name):
-        print("Good {} {} .How are you today!".format(daytime,name))
+        #print("Good {} {} .How are you today!".format(daytime,name))
+        print(f"Good {daytime} {name} .How are you today!")
 
 greetings("afternoon","Anu")
-greetings("morning","Srijan")
+# greetings("morning","Srijan")
 
 my_word="Pen"
 rev_word=my_word[::-1]
 print(rev_word)
 
-def rev_word(word):
-    my_rev_word = word[::-1]
-    return my_rev_word
-
-print(rev_word("abcdefghijk"))
-
+# def rev_word(word):
+#     my_rev_word = word[::-1]
+#     return my_rev_word
+#
+# print(rev_word("abcdefghijk"))
+#
